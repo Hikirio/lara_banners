1ffb8f17504e4ba7a0dd8e60a92871d4d7eef99c
anato <anatoliy.matvienkov38@gmail.com>
2020-02-14 19:36:48 +0200

    create design admin

--- app/Banner.php
+++ app/Banner.php
@@ -9,4 +9,9 @@ class Banner extends Model
     protected $fillable = [
         'title', 'image', 'url', 'status', 'position',
     ];
+
+    public function find_all()
+    {
+        return Banner::all();
+    }
 }
